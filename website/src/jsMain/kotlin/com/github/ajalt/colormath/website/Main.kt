cebc5e3e723ce6da32a3b0ae024371b0ce71d837
AJ <aj@alt>
2021-06-15 10:48:01 -0700

    Update tryit to latest API

--- website/src/jsMain/kotlin/com/github/ajalt/colormath/website/Main.kt
+++ website/src/jsMain/kotlin/com/github/ajalt/colormath/website/Main.kt
@@ -4,15 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.github.ajalt.colormath.Color
+import com.github.ajalt.colormath.CssColors
+import com.github.ajalt.colormath.RGB
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.events.WrappedEvent
 import org.jetbrains.compose.web.renderComposable
-import com.github.ajalt.colormath.Color
-import com.github.ajalt.colormath.CssColors
-import com.github.ajalt.colormath.RGB
-import kotlinx.browser.document
 import kotlin.js.json
 
 fun main() {
@@ -21,8 +20,8 @@ fun main() {
     @Composable
     fun row(
         title: String,
-        vararg a: Pair<Number, (Double) -> Color>,
-        int: Boolean = false,
+        vararg a: Pair<Number, (Float) -> Color>,
+        intScale: Int? = null,
     ) {
         P {
             Div(attrs = {
@@ -32,7 +31,7 @@ fun main() {
                 }
             }) { Text(title) }
             for ((get, set) in a) {
-                Input(InputType.Number, value = fmt(get), attrs = {
+                Input(InputType.Number, value = fmt(get.toDouble() * (intScale ?: 1)), attrs = {
                     style {
                         property("max-width", value(20.percent))
                         property("appearance", value("textfield"))
@@ -41,9 +40,8 @@ fun main() {
                         property("padding-right", value(1.rem))
                     }
                     classes("md-search__input") // class is part of mkdocs material
-                    attr("step", if (int) "1" else "0.1")
-                    onChange { color = set(it.stringValue.toDouble()) }
-                    onTextInput { }
+                    attr("step", if (intScale == null) "0.1" else "1")
+                    onChange { color = set(it.stringValue.toFloat() / (intScale ?: 1)) }
                 })
             }
         }
@@ -65,38 +63,35 @@ fun main() {
             }
             row(
                 "RGB",
-                color.toRGB().r to { color.toRGB().copy(r = it.toInt()) },
-                color.toRGB().g to { color.toRGB().copy(g = it.toInt()) },
-                color.toRGB().b to { color.toRGB().copy(b = it.toInt()) },
-                int = true,
+                color.toRGB().r to { color.toRGB().copy(r = it) },
+                color.toRGB().g to { color.toRGB().copy(g = it) },
+                color.toRGB().b to { color.toRGB().copy(b = it) },
+                intScale = 255,
             )
             row(
                 "CMYK",
-                color.toCMYK().c to { color.toCMYK().copy(c = it.toInt()) },
-                color.toCMYK().m to { color.toCMYK().copy(m = it.toInt()) },
-                color.toCMYK().y to { color.toCMYK().copy(y = it.toInt()) },
-                color.toCMYK().k to { color.toCMYK().copy(k = it.toInt()) },
-                int = true,
+                color.toCMYK().c to { color.toCMYK().copy(c = it) },
+                color.toCMYK().m to { color.toCMYK().copy(m = it) },
+                color.toCMYK().y to { color.toCMYK().copy(y = it) },
+                color.toCMYK().k to { color.toCMYK().copy(k = it) },
             )
             row(
                 "HSL",
-                color.toHSL().h to { color.toHSL().copy(h = it.toInt()) },
-                color.toHSL().s to { color.toHSL().copy(s = it.toInt()) },
-                color.toHSL().l to { color.toHSL().copy(l = it.toInt()) },
-                int = true,
+                color.toHSL().h to { color.toHSL().copy(h = it) },
+                color.toHSL().s to { color.toHSL().copy(s = it) },
+                color.toHSL().l to { color.toHSL().copy(l = it) },
             )
             row(
                 "HSV",
-                color.toHSV().h to { color.toHSV().copy(h = it.toInt()) },
-                color.toHSV().s to { color.toHSV().copy(s = it.toInt()) },
-                color.toHSV().v to { color.toHSV().copy(v = it.toInt()) },
-                int = true,
+                color.toHSV().h to { color.toHSV().copy(h = it) },
+                color.toHSV().s to { color.toHSV().copy(s = it) },
+                color.toHSV().v to { color.toHSV().copy(v = it) },
             )
             row(
                 "HWB",
-                color.toHWB().h to { color.toHWB().copy(h = it.toFloat()) },
-                color.toHWB().w to { color.toHWB().copy(w = it.toFloat()) },
-                color.toHWB().b to { color.toHWB().copy(b = it.toFloat()) },
+                color.toHWB().h to { color.toHWB().copy(h = it) },
+                color.toHWB().w to { color.toHWB().copy(w = it) },
+                color.toHWB().b to { color.toHWB().copy(b = it) },
             )
             row(
                 "LAB",
@@ -106,15 +101,15 @@ fun main() {
             )
             row(
                 "LCH",
-                color.toLCH().l to { color.toLCH().copy(l = it.toFloat()) },
-                color.toLCH().c to { color.toLCH().copy(c = it.toFloat()) },
-                color.toLCH().h to { color.toLCH().copy(h = it.toFloat()) },
+                color.toLCH().l to { color.toLCH().copy(l = it) },
+                color.toLCH().c to { color.toLCH().copy(c = it) },
+                color.toLCH().h to { color.toLCH().copy(h = it) },
             )
             row(
                 "LUV",
-                color.toLUV().l to { color.toLUV().copy(l = it.toFloat()) },
-                color.toLUV().u to { color.toLUV().copy(u = it.toFloat()) },
-                color.toLUV().v to { color.toLUV().copy(v = it.toFloat()) },
+                color.toLUV().l to { color.toLUV().copy(l = it) },
+                color.toLUV().u to { color.toLUV().copy(u = it) },
+                color.toLUV().v to { color.toLUV().copy(v = it) },
             )
             row(
                 "XYZ",
@@ -125,12 +120,12 @@ fun main() {
             row(
                 "Ansi-16",
                 color.toAnsi16().code to { color.toAnsi16().copy(code = it.toInt()) },
-                int = true,
+                intScale = 1,
             )
             row(
                 "Ansi-256",
                 color.toAnsi256().code to { color.toAnsi256().copy(code = it.toInt()) },
-                int = true,
+                intScale = 1,
             )
             Div(attrs = {
                 style {
