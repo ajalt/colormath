e9a925b9d3f15b5941f26c7ecd593ba08082d8df
AJ <aj@alt>
2021-06-16 09:08:15 -0700

    Add lerp function

--- src/commonMain/kotlin/com/github/ajalt/colormath/HueColor.kt
+++ src/commonMain/kotlin/com/github/ajalt/colormath/HueColor.kt
@@ -1,7 +1,5 @@
 package com.github.ajalt.colormath
 
-import kotlin.math.PI
-
 interface HueColor {
     /** The hue, as degrees in the range `[0, 360]` */
     val h: Float
@@ -15,14 +13,3 @@ fun HueColor.hueAsRad(): Float = h.degToRad()
 
 /** Convert this color's hue to turns (360° == 1 turn) */
 fun HueColor.hueAsTurns(): Float = h.degToTurns()
-
-internal fun Float.degToRad(): Float = (this * PI / 180f).toFloat()
-internal fun Float.radToDeg(): Float = (this * 180f / PI).toFloat()
-internal fun Float.gradToDeg(): Float = this * .9f
-internal fun Float.turnToDeg(): Float = this * 360f
-internal fun Float.degToGrad(): Float = this * 200f / 180f
-internal fun Float.degToTurns(): Float = this / 360f
-
-/** Return this value shifted to lie in [0, 360] */
-// formula from https://www.w3.org/TR/css-color-4/#hue-interpolation
-internal fun Float.normalizeDeg(): Float = ((this % 360) + 360) % 360 //
