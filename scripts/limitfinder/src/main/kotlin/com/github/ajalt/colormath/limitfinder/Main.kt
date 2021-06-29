6203f5fc1dfb8841a4e871faf67346a03691877f
AJ <aj@alt>
2021-06-29 10:41:26 -0700

    Add limitfinder tool

---
+++ scripts/limitfinder/src/main/kotlin/com/github/ajalt/colormath/limitfinder/Main.kt
@@ -0,0 +1,36 @@
+package com.github.ajalt.colormath.limitfinder
+
+import com.github.ajalt.colormath.*
+
+fun main() {
+    val colors = listOf(
+        HCL(0f, 0f, 0f),
+        LAB(0f, 0f, 0f),
+        LCH(0f, 0f, 0f),
+        LUV(0f, 0f, 0f),
+        Oklab(0f, 0f, 0f),
+        Oklch(0f, 0f, 0f),
+        XYZ(0f, 0f, 0f),
+    )
+    for (color in colors) {
+        println("Computing sRGB range for ${color.model.name}")
+        val mins = FloatArray(color.model.components.size - 1) { Float.MAX_VALUE }
+        val maxs = FloatArray(color.model.components.size - 1) { Float.MIN_VALUE }
+        for (r in 0..255) {
+            for (g in 0..255) {
+                for (b in 0..255) {
+                    val c = color.convertToThis(RGB(r, g, b)).components()
+                    for (i in mins.indices) {
+                        mins[i] = minOf(mins[i], c[i])
+                        maxs[i] = maxOf(maxs[i], c[i])
+                    }
+                }
+            }
+        }
+
+        color.model.components.dropLast(1).forEachIndexed { i, it ->
+            println(" ${it.name}: [${mins[i]}, ${maxs[i]}]")
+        }
+        println()
+    }
+}
