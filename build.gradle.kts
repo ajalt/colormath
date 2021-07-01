8bfa7801172d41b35a6d502f90f74cf0499dcd96
AJ <aj@alt>
2021-07-01 11:39:23 -0700

    Update compose pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.dokka.base.DokkaBaseConfiguration
 import java.io.ByteArrayOutputStream
 
 plugins {
-    kotlin("multiplatform") version "1.5.20"
+    kotlin("multiplatform") version "1.5.10"
     id("org.jetbrains.dokka") version "1.4.32"
     id("maven-publish")
     id("signing")
