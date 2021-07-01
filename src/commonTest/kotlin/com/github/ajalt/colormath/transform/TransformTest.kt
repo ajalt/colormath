1cc1ca5a3ef1ec237b6fbb8cb878159ca95e35fc
AJ <aj@alt>
2021-07-01 11:11:17 -0700

    Add interpolator tests

--- src/commonTest/kotlin/com/github/ajalt/colormath/transform/TransformTest.kt
+++ src/commonTest/kotlin/com/github/ajalt/colormath/transform/TransformTest.kt
@@ -1,12 +1,11 @@
 package com.github.ajalt.colormath.transform
 
 import com.github.ajalt.colormath.RGB
-import com.github.ajalt.colormath.convertTo
 import com.github.ajalt.colormath.shouldEqualColor
 import io.kotest.data.blocking.forAll
 import io.kotest.data.row
-import io.kotest.matchers.should
-import io.kotest.matchers.types.shouldBeTypeOf
+import io.kotest.inspectors.forAll
+import kotlin.jvm.JvmName
 import kotlin.test.Test
 
 class TransformTest {
@@ -18,6 +17,63 @@ class TransformTest {
         row(RGB(0, 0, 0), RGB(254, 254, 254).toXYZ(), 1f, RGB(254, 254, 254)),
     ) { c1, c2, a, ex ->
         c1.interpolate(c2, a).shouldEqualColor(ex)
+        c1.model.interpolator(c1, c2).interpolate(a).shouldEqualColor(ex)
+    }
+
+    @Test
+    @JvmName("interpolator_with_hint")
+    fun `interpolator with hint`() = forAll(
+        row(0.00, RGB("#000")),
+        row(0.25, RGB("#400")),
+        row(0.50, RGB("#800")),
+        row(0.55, RGB("#822")),
+        row(0.60, RGB("#844")),
+        row(0.80, RGB("#866")),
+        row(1.00, RGB("#888")),
+    ) { pos, ex ->
+        RGB.interpolator {
+            stop(RGB("#000"))
+            stop(RGB("#800"))
+            hint(.6)
+            stop(RGB("#888"))
+        }.interpolate(pos).shouldEqualColor(ex)
+    }
+
+    @Test
+    @JvmName("interpolator_explicit_positions")
+    fun `interpolator with explicit positions`() = forAll(
+        row(0.00, RGB("#111")),
+        row(0.15, RGB("#222")),
+        row(0.20, RGB("#333")),
+        row(0.50, RGB("#333")),
+        row(0.70, RGB("#333")),
+        row(0.90, RGB("#555")),
+        row(1.00, RGB("#555")),
+    ) { pos, ex ->
+        RGB.interpolator {
+            stop(RGB("#111"), .1)
+            stop(RGB("#333"), .2, .8)
+            stop(RGB("#555"), .8)
+        }.interpolate(pos).shouldEqualColor(ex)
+    }
+
+    @Test
+    @JvmName("interpolator_sequence")
+    fun `interpolator sequence`() {
+        RGB.interpolator(RGB("#000"), RGB("#888")).sequence(9)
+            .toList().zip(listOf(
+                RGB("#000"),
+                RGB("#111"),
+                RGB("#222"),
+                RGB("#333"),
+                RGB("#444"),
+                RGB("#555"),
+                RGB("#666"),
+                RGB("#777"),
+                RGB("#888"),
+            )).forAll { (a, ex) ->
+                a.shouldEqualColor(ex)
+            }
     }
 
     @Test
@@ -26,7 +82,7 @@ class TransformTest {
         row(RGB(100, 100, 100, 0.5f), RGB(50, 50, 50, 0.5f)),
         row(RGB(100, 100, 100, 0f), RGB(0, 0, 0, 0f)),
     ) { rgb, ex ->
-       rgb.multiplyAlpha().shouldEqualColor(ex)
+        rgb.multiplyAlpha().shouldEqualColor(ex)
     }
 
     @Test
