3ef71eda7a2d84f73979e296716eaf68eaab9e82
AJ <aj@alt>
2021-07-05 10:01:16 -0700

    Update gradient sample

--- website/gradient/src/jsMain/kotlin/com/github/ajalt/colormath/website/Main.kt
+++ website/gradient/src/jsMain/kotlin/com/github/ajalt/colormath/website/Main.kt
@@ -4,79 +4,112 @@ package com.github.ajalt.colormath.website
 
 import androidx.compose.runtime.*
 import com.github.ajalt.colormath.*
+import com.github.ajalt.colormath.Color
 import com.github.ajalt.colormath.transform.interpolator
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.attributes.value
-import org.jetbrains.compose.web.css.borderRadius
-import org.jetbrains.compose.web.css.margin
-import org.jetbrains.compose.web.css.marginTop
-import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H4
-import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.RangeInput
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 import org.khronos.webgl.Uint8ClampedArray
 import org.w3c.dom.HTMLCanvasElement
+import kotlin.js.json
+
+private val rectColors = listOf(RGB, LAB, LUV, Oklab)
 
 fun main() {
-    var color1: Color by mutableStateOf(Color.parse("rebeccapurple"))
-    var color2: Color by mutableStateOf(Color.parse("aliceblue"))
+    var colors by mutableStateOf(listOf(RGB("#0000cc"), Color.parse("aliceblue")))
 
     @Composable
     fun row(model: ColorModel<*>) {
-        Div {
-            H4(attrs = {
-                style { property("margin-bottom", 0.px) }
+        Div(attrs = {
+            style {
+                display(DisplayStyle.Flex)
+                alignItems(AlignItems.Center)
+                justifyContent(JustifyContent.Center)
+                flexWrap(FlexWrap.Wrap)
+                margin(4.px)
+            }
+        }) {
+            Div(attrs = {
+                style {
+                    width(5.em)
+                }
             }) { Text(model.name) }
             Canvas(attrs = {
                 this.attr("width", "800")
-                this.attr("height", "80")
+                this.attr("height", "60")
 
                 style {
-                    marginTop(16.px)
                     borderRadius(4.px)
                 }
             }) {
-                DomSideEffect(color1 to color2) { updateCanvas(it, model.convert(color1), model.convert(color2)) }
+                DomSideEffect(colors) { updateCanvas(it, model.convert(colors[0]), model.convert(colors[1])) }
             }
         }
     }
 
     renderComposable(rootElementId = "root") {
 
-        Div(attrs = { style { margin(16.px) } }) {
-            Div {
-                Input(InputType.Color, attrs = {
-                    value(color1.toRGB().toHex())
-                    onInput { color1 = RGB(it.value) }
-                })
-                Input(InputType.Color, attrs = {
-                    value(color2.toRGB().toHex())
-                    onInput { color2 = RGB(it.value) }
-                })
+        Div(attrs = {
+            style {
+                display(DisplayStyle.Flex)
+                flexWrap(FlexWrap.Wrap)
+                justifyContent(JustifyContent.SpaceEvenly)
+            }
+        }) {
+            for ((i, color) in colors.withIndex()) {
+                Div {
+                    for (j in 0 until color.model.components.lastIndex) {
+                        val component = color.model.components[j]
+                        val value = color.toArray()[j]
+                        Div(attrs = {
+                            style {
+                                display(DisplayStyle.Flex)
+                                flexWrap(FlexWrap.Wrap)
+                            }
+                        }) {
+                            Div(attrs = { style { width(2.em) } }) { Text(component.name) }
+                            Div(attrs = { style { width(3.em) } }) { Text(fmt(value)) }
+                            Div {
+                                RangeInput(value,
+                                    min = component.min,
+                                    max = component.max,
+                                    step = 0.01,
+                                    attrsBuilder = {
+                                        onInput {
+                                            val array = color.toArray().apply { set(j, (it.value ?: 0).toFloat()) }
+                                            val new = color.model.create(array)
+                                            colors = if (i == 0) listOf(new, colors[1]) else listOf(colors[0], new)
+                                        }
+                                    })
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        row(RGB)
-        row(LAB)
-        row(LUV)
-        row(Oklab)
+        rectColors.forEach { row(it) }
     }
 }
 
 private fun updateCanvas(canvas: HTMLCanvasElement, color1: Color, color2: Color) {
-    val lerp = RGB.interpolator(color1, color2)
+    val lerp = color1.model.interpolator(color1, color2)
     canvas.edit2dImageData {
-        repeat(width) { x ->
-            repeat(height) { y ->
-                data.setColor(x, y, width, lerp.interpolate(x / width.toFloat()))
-            }
+        // draw the first row
+        for (x in 0 until width) {
+            data.setColor(x, 0, width, lerp.interpolate(x / width.toFloat()))
+        }
+        // copy the first row to the rest of the canvas
+        for (y in 0 until height) {
+            data.asDynamic().copyWithin(y * width * 4, 0, width * 4)
         }
     }
 }
 
-private fun Uint8ClampedArray.setColor(x: Int, y: Int, width: Int, rgb: RGB) {
+private fun Uint8ClampedArray.setColor(x: Int, y: Int, width: Int, color: Color) {
+    val rgb = color.toRGB()
     val i = (y * width + x) * 4
     with(asDynamic()) {
         this[i] = rgb.redInt
@@ -84,4 +117,10 @@ private fun Uint8ClampedArray.setColor(x: Int, y: Int, width: Int, rgb: RGB) {
         this[i + 2] = rgb.blueInt
         this[i + 3] = rgb.alphaInt
     }
+}
+
+private fun fmt(number: Number): String {
+    return number.asDynamic().toLocaleString("en-us",
+        json("useGrouping" to false, "maximumFractionDigits" to 3)
+    ).unsafeCast<String>()
 }
