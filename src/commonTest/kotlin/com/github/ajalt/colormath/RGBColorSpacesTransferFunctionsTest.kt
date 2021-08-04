475d84f2e6c3fef4a30b737f26edf0d5b5bb0e48
AJ <aj@alt>
2021-08-04 14:12:46 -0700

    Add more cctf tests

--- src/commonTest/kotlin/com/github/ajalt/colormath/RGBColorSpacesTransferFunctionsTest.kt
+++ src/commonTest/kotlin/com/github/ajalt/colormath/RGBColorSpacesTransferFunctionsTest.kt
@@ -19,16 +19,16 @@ import kotlin.test.Test
 // Test values from https://github.com/colour-science/
 class RGBColorSpacesTransferFunctionsTest {
     @Test
-    fun SRGB() = doTest(SRGB, 0.46135612950044164)
+    fun SRGB() = doTest(SRGB, 0.01292, 0.46135612950044164)
 
     @Test
-    fun ADOBE_RGB() = doTest(ADOBE_RGB, 0.45852946567989455)
+    fun ADOBE_RGB() = doTest(ADOBE_RGB, 0.043239356144868332, 0.45852946567989455)
 
     @Test
-    fun BT_2020() = doTest(BT_2020, 0.40884640249350368)
+    fun BT_2020() = doTest(BT_2020, 0.0045, 0.40884640249350368)
 
     @Test
-    fun BT_709() = doTest(BT_709, 0.409007728864150)
+    fun BT_709() = doTest(BT_709, 0.0045, 0.409007728864150)
 
     @Test
     fun BT_709_extra() = forAll(
@@ -39,28 +39,30 @@ class RGBColorSpacesTransferFunctionsTest {
     }
 
     @Test
-    fun DCI_P3() = doTest(DCI_P3, 0.5170902489415321)
+    fun DCI_P3() = doTest(DCI_P3, 0.070170382867038292, 0.5170902489415321)
 
     @Test
-    fun DISPLAY_P3() = doTest(DISPLAY_P3, 0.46135612950044164)
+    fun DISPLAY_P3() = doTest(DISPLAY_P3, 0.01292, 0.46135612950044164)
 
     @Test
-    fun ROMM_RGB() = doTest(ROMM_RGB, 0.385711424751138)
+    fun ROMM_RGB() = doTest(ROMM_RGB, 0.016, 0.385711424751138)
 
     @Test
-    fun ACEScc() = doTest(ACEScc, 0.413588402492442, -0.358447488584475, 0.554794520547945)
+    fun ACEScc() = doTest(ACEScc, -0.01402878337112365, 0.413588402492442, -0.358447488584475, 0.554794520547945)
 
     @Test
-    fun ACEScct() = doTest(ACEScct, 0.413588402492442, 0.072905534195835495, 0.554794520547945)
+    fun ACEScct() = doTest(ACEScct, 0.08344577193748999, 0.413588402492442, 0.072905534195835495, 0.554794520547945)
 
-    private fun doTest(space: RGBColorSpace, mid: Double, zero: Double = 0.0, one: Double = 1.0) {
+    private fun doTest(space: RGBColorSpace, zzOne: Double, eighteen: Double, zero: Double = 0.0, one: Double = 1.0) {
         forAll(
             row(0.0, zero, "oetf"),
-            row(0.18, mid, "oetf"),
+            row(0.001, zzOne, "oetf"),
+            row(0.18, eighteen, "oetf"),
             row(1.0, one, "oetf"),
 
             row(zero, 0.0, "eotf"),
-            row(mid, 0.18, "eotf"),
+            row(zzOne, 0.001, "eotf"),
+            row(eighteen, 0.18, "eotf"),
             row(one, 1.0, "eotf"),
         ) { input, ex, func ->
             doSingleTest(space, input, ex, func)
