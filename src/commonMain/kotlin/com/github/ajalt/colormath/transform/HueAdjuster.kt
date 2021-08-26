6f78fe356e295fec8778cbcfcd3a421056b7603a
AJ <aj@alt>
2021-08-26 10:04:10 -0700

    Change HueAdjustment to operate on all hues

--- src/commonMain/kotlin/com/github/ajalt/colormath/transform/HueAdjuster.kt
+++ src/commonMain/kotlin/com/github/ajalt/colormath/transform/HueAdjuster.kt
@@ -1,33 +1,50 @@
 package com.github.ajalt.colormath.transform
 
+import com.github.ajalt.colormath.internal.normalizeDeg
 import kotlin.math.absoluteValue
 import kotlin.math.withSign
 
 /**
- * A function that takes the difference between two hue angles and returns the new difference that should be used.
- *
- * The delta and return value are specified in degrees in the range `(-360, 360)`.
+ * A function that takes a list of angles in degrees and returns a new list with the angles adjusted
+ * to the values that should be used for interpolation.
  */
-typealias HueAdjustment = (delta: Float) -> Float
+typealias HueAdjustment = (hues: List<Float>) -> List<Float>
 
 object HueAdjustments {
     /** Angles are adjusted so that their difference is in `[-180, 180]` */
-    val shorter: HueAdjustment = {
+    val shorter: HueAdjustment = deltaAdjustment {
         if (it.absoluteValue <= 180) it else it - 360f.withSign(it)
     }
 
     /** Angles are adjusted so that their difference is 0 or is in `[180, 360)` */
-    val longer: HueAdjustment = {
+    val longer: HueAdjustment = deltaAdjustment {
         if (it == 0f || it.absoluteValue >= 180) it else it - 360f.withSign(it)
     }
 
     /** Angles are adjusted so that their difference is in `[0, 360)` */
-    val increasing: HueAdjustment = {
+    val increasing: HueAdjustment = deltaAdjustment {
         if (it >= 0) it else it + 360f
     }
 
     /** Angles are adjusted so that their difference is in `(-360, 0]` */
-    val decreasing: HueAdjustment = {
+    val decreasing: HueAdjustment = deltaAdjustment {
         if (it <= 0) it else it - 360f
+    }
+
+    /**
+     * Leave all angles unchanged
+     */
+    val specified: HueAdjustment = { it }
+}
+
+private inline fun deltaAdjustment(crossinline adj: (delta: Float) -> Float): HueAdjustment = { hues ->
+    hues.toMutableList().also { h ->
+        h[0] = h[0].normalizeDeg()
+        for (i in 1..h.lastIndex) {
+            val hue = h[i]
+            val prev = h[i - 1]
+            if (hue.isNaN() || prev.isNaN()) continue
+            h[i] = prev + adj(hue.normalizeDeg() - prev.normalizeDeg())
+        }
     }
 }
--- src/commonTest/kotlin/com/github/ajalt/colormath/transform/HueAdjustmentsTest.kt
+++ src/commonTest/kotlin/com/github/ajalt/colormath/transform/HueAdjustmentsTest.kt
@@ -7,6 +7,12 @@ import kotlin.test.Test
 
 class HueAdjustmentsTest {
     @Test
+    fun nan() {
+        val l = listOf(0f, Float.NaN, 10f, 20f, Float.NaN, Float.NaN)
+        HueAdjustments.shorter(l) shouldBe l
+    }
+
+    @Test
     fun shorter() = doTest(
         HueAdjustments.shorter,
         listOf(0, 300, 50, 0, 100),
@@ -36,12 +42,12 @@ class HueAdjustmentsTest {
 
     @Test
     fun specified() = doTest(
-        null,
+        HueAdjustments.specified,
         listOf(0, 300, 50, 0, 100),
         listOf(0, 300, 50, 0, 100),
     )
 
-    private fun doTest(adj: HueAdjustment?, before: List<Int>, expected: List<Int>) {
+    private fun doTest(adj: HueAdjustment, before: List<Int>, expected: List<Int>) {
         val lerp = HSL.interpolator {
             hueAdjustment = adj
             before.forEach { stop(HSL(it.toDouble(), .5, .5)) }
