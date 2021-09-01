d78e049d81cfb4dd005ca38c0c9c3e8d25755500
AJ <aj@alt>
2021-09-01 11:40:23 -0700

    update gradient sample

--- website/gradient/src/jsMain/kotlin/com/github/ajalt/colormath/website/Elements.kt
+++ website/gradient/src/jsMain/kotlin/com/github/ajalt/colormath/website/Elements.kt
@@ -14,12 +14,18 @@ import org.w3c.dom.ImageData
 
 @Composable
 fun Canvas(
+    width: Int,
+    height: Int,
     attrs: AttrBuilderContext<HTMLCanvasElement> = {},
     fallbackContent: ContentBuilder<HTMLCanvasElement>? = null,
 ) {
     TagElement(
         elementBuilder = ElementBuilder.createBuilder("canvas"),
-        applyAttrs = attrs,
+        applyAttrs = {
+            attr("width", width.toString())
+            attr("height", height.toString())
+            attrs()
+        },
         content = fallbackContent
     )
 }
