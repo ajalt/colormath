649d29126bc086b06f231a4d543cbc579a975b87
AJ <aj@alt>
2021-09-07 08:55:24 -0700

    Add cubic bezier benchmark

--- scripts/benchmarks/src/jmh/kotlin/com/github/ajalt/colormath/benchmark/ColorBenchmarks.kt
+++ scripts/benchmarks/src/jmh/kotlin/com/github/ajalt/colormath/benchmark/ColorBenchmarks.kt
@@ -1,6 +1,7 @@
 package com.github.ajalt.colormath.benchmark
 
 import com.github.ajalt.colormath.*
+import com.github.ajalt.colormath.transform.EasingFunctions
 import com.github.ajalt.colormath.transform.createChromaticAdapter
 import com.github.ajalt.colormath.transform.interpolator
 import com.github.ajalt.colormath.transform.sequence
@@ -12,6 +13,7 @@ private val interpolator = RGB.interpolator(RGB("#000a"), RGB("#fffa"), premulti
 private val interpolatorPrumult = RGB.interpolator(RGB("#000a"), RGB("#fffa"))
 private val adapter = RGBInt.createChromaticAdapter(RGBInt(200, 210, 220))
 private val rgbInt = RGBInt(11, 222, 33)
+private val bezier = EasingFunctions.cubicBezier(0.3, -1, 0.7, 2)
 
 @Warmup(iterations = 3, time = 1)
 @Measurement(iterations = 3, time = 1)
@@ -42,17 +44,7 @@ open class ColorBenchmarks {
     }
 
     @Benchmark
-    open fun rgbToLch(): LCHab {
-        return RGB(0.3f, 0.4f, 0.6f).toLCHab()
-    }
-
-    @Benchmark
-    open fun rgbToLinear(): RGB {
-        return RGB(0.3f, 0.4f, 0.6f).convertTo(RGBColorSpaces.LINEAR_SRGB)
-    }
-
-    @Benchmark
-    open fun chromaticAdapter(): Int {
-        return adapter.adapt(rgbInt).argb.toInt()
+    open fun cubicBezier(): Float {
+        return bezier.ease(0.2f)
     }
 }
