c390fc76762b30b7490ab87965da84a343530fd2
AJ <aj@alt>
2021-09-07 09:09:56 -0700

    Update to kotlin 1.5.30

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,11 @@ include("website:converter")
 include("website:gradient")
 include("scripts:benchmarks")
 
-// For compose web
+// For compose web and dokka dev
 pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
     }
 }
