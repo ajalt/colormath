a74a94060905e708f921314490d6221d38115602
AJ <aj@alt>
2023-06-22 11:00:01 -0700

    Move version catalog to toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,27 +25,4 @@ dependencyResolutionManagement {
         google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    versionCatalogs {
-        create("libs") {
-            version("kotlin", "1.8.20")
-            version("jbCompose", "1.4.0")
-
-            plugin("dokka", "org.jetbrains.dokka").version("1.8.10")
-            plugin("publish", "com.vanniktech.maven.publish").version("0.25.2")
-
-            // used in tests
-            library("kotest", "io.kotest:kotest-assertions-core:5.6.1")
-
-            // used in extensions
-            plugin("android-library", "com.android.library").version("7.4.0")
-
-            library("compose-ui-graphics", "org.jetbrains.compose.ui", "ui-graphics").versionRef("jbCompose")
-            library("androidx-annotation", "androidx.annotation:annotation:1.6.0")
-            library("junit", "junit:junit:4.13.2")
-            library("robolectric", "org.robolectric:robolectric:4.10")
-
-            // used in samples
-            plugin("compose", "org.jetbrains.compose").versionRef("jbCompose")
-        }
-    }
 }
