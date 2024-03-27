339b284e6d37432a89b449f93b287746a9abc2fd
AJ <aj@alt>
2024-03-27 12:52:18 -0700

    Run compose tests on JVM

--- extensions/colormath-ext-jetpack-compose/src/jvmTest/kotlin/com/github/ajalt/colormath/extensions/android/colorint/ComposeColorExtensionsTest.kt
+++ extensions/colormath-ext-jetpack-compose/src/jvmTest/kotlin/com/github/ajalt/colormath/extensions/android/colorint/ComposeColorExtensionsTest.kt
@@ -1,31 +1,27 @@
 package com.github.ajalt.colormath.extensions.android.colorint
 
 import androidx.compose.ui.graphics.Color
-import org.junit.Assert.*
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 import com.github.ajalt.colormath.extensions.android.composecolor.toColormathColor
 import com.github.ajalt.colormath.extensions.android.composecolor.toColormathSRGB
 import com.github.ajalt.colormath.extensions.android.composecolor.toComposeColor
 import com.github.ajalt.colormath.model.RGB
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [26])
 class ComposeColorExtensionsTest {
     private val colormathBlue = RGB(0, 0, 1, 1)
 
-    @org.junit.Test
+    @Test
     fun toColormathColor() {
         assertEquals(colormathBlue, Color.Blue.toColormathColor())
     }
 
-    @org.junit.Test
+    @Test
     fun toColormathSRGB() {
         assertEquals(colormathBlue, Color.Blue.toColormathSRGB())
     }
 
-    @org.junit.Test
+    @Test
     fun toComposeColor() {
         assertEquals(Color.Blue, colormathBlue.toComposeColor())
     }
